035bbc8921b63ad2e43e97bdacff6948c04aa3e5
Alex Schin <alschin@endava.net>
2016-06-01 17:14:52 +0300

    finished ex.3 started ex.4

--- c4e3.js
+++ c4e3.js
@@ -14,7 +14,7 @@ const arrayToList = (array) => {
 
 const listToArray = (list) => {
   let array = []
-  for (var node = list; node; node = node.rest) {
+  for (let node = list; node; node = node.rest) {
     array.push(node.value)
   }
   return array
@@ -26,13 +26,23 @@ const prepend = (value, list) => {
 }
 
 const nth = (list, index) => {
-  for (var node = list, i = 0; i <= index; node = node.rest, i++) {
+  for (let node = list, i = 0; i <= index && node; node = node.rest, i++) {
     if (i === index) {
       return node.value
     }
   }
 }
 
+const recursiveNth = (list, index) => {
+  if (!(index < 0)) {
+    if (index > 0 && list.rest) {
+      return recursiveNth(list.rest, index - 1)
+    } else {
+      return list.value
+    }
+  }
+}
+
 console.log(listToArray(arrayToList([1, 2, 3, 4, 5])))
 console.log(prepend(10, prepend(20, null)))
-console.log(nth(arrayToList([10, 20, 30]), 1))
+console.log(recursiveNth(arrayToList([10, 20, 30]), -1))
---
+++ c4e4.js
@@ -0,0 +1,18 @@
+'use strict'
+
+// --------------------
+// Deep comparison
+// --------------------
+const queue = () => {
+  this.enqueue = () => {
+    console.log('yey!')
+  }
+}
+
+const deepEqual = (obj1, obj2) => {
+  let equal = true
+  for (let property of obj1) {
+
+  }
+  return equal
+}
